83c7f21dfe6cef88cb7f5b3c4dfb2caf6be0779d
Daniel Roth <daniel.roth@mailbase.info>
2019-04-24 07:48:59 +0200

    adds distance joint
    * a ball joint is a special more optimized form with length 0

---
+++ src/simulation/constraints/DistanceJoint.cpp
@@ -0,0 +1,107 @@
+﻿#include "DistanceJoint.h"
+#include "ConstraintVisitor.h"
+
+#include <simulation/ImpulseConstraintSolver.h>
+
+#include <iostream>
+
+DistanceJoint::DistanceJoint(
+        SimulationPointRef pointA,
+        SimulationPointRef pointB,
+        double distance)
+    : mPointA(pointA)
+    , mPointB(pointB)
+    , mDistance(distance)
+{
+    mTargetURel = Eigen::Vector::Zero();
+}
+
+const Eigen::Vector& DistanceJoint::getTargetURel() const
+{
+    return mTargetURel;
+}
+
+const Eigen::Vector& DistanceJoint::getSumOfAllAppliedImpulses() const
+{
+    return mSumOfAllAppliedImpulses;
+}
+
+void DistanceJoint::setSumOfAllAppliedImpulses(const Eigen::Vector& impulses)
+{
+    mSumOfAllAppliedImpulses = impulses;
+}
+
+void DistanceJoint::initialize(double stepSize)
+{
+    mStepSize = stepSize;
+    mImpulseFactor = (ImpulseConstraintSolver::calculateK(mPointB) +
+                      ImpulseConstraintSolver::calculateK(mPointA)).inverse();
+
+    mTargetURel = Eigen::Vector::Zero();
+
+    if (mDistance > 1e-10)
+    {
+        Eigen::Vector direction = mPointA.getPoint() - mPointB.getPoint();
+        if (direction.norm() < 1e-10)
+            direction = Eigen::Vector(1.0, 0.0, 0.0);
+        else
+            direction.normalize();
+
+        mTargetURel = mDistance * direction / stepSize;
+    }
+
+    mTargetURel -= (mPointA.getPoint() - mPointB.getPoint() ) / stepSize;
+}
+
+bool DistanceJoint::solve(double maxConstraintError)
+{
+    Eigen::Vector p1 = ImpulseConstraintSolver::calculateRelativePoint(
+                mPointA.getSimulationObject(), mPointA.getPoint());
+    Eigen::Vector p2 = ImpulseConstraintSolver::calculateRelativePoint(
+                mPointB.getSimulationObject(), mPointB.getPoint());
+
+    Eigen::Vector v1 = ImpulseConstraintSolver::calculateSpeed(
+                mPointA.getSimulationObject(), p1, mPointA.getIndex());
+    Eigen::Vector v2 = ImpulseConstraintSolver::calculateSpeed(
+                mPointB.getSimulationObject(), p2, mPointB.getIndex());
+
+    Eigen::Vector uRel = v1 - v2;
+
+    // project uRel on sphere
+    Eigen::Vector p1Next = mPointA.getPoint();// + mStepSize * v1;
+    Eigen::Vector p2Next = mPointB.getPoint();// + mStepSize * v2;
+
+    Eigen::Vector direction = p2Next - p1Next;
+    if (direction.norm() < 1e-10)
+        direction = Eigen::Vector(1.0, 0.0, 0.0);
+    else
+        direction.normalize();
+
+    Eigen::Vector deltaURel = mTargetURel - uRel;
+    deltaURel = deltaURel.dot(direction) * direction;
+
+    if (deltaURel.norm() < maxConstraintError)
+    {
+        return true;
+    }
+
+    Eigen::Vector impulse = mImpulseFactor * deltaURel;
+
+    ImpulseConstraintSolver::applyImpulse(
+                mPointA.getSimulationObject(), impulse, p1, mPointA.getIndex());
+    ImpulseConstraintSolver::applyImpulse(
+                mPointB.getSimulationObject(), -impulse, p2, mPointB.getIndex());
+
+    return false;
+}
+
+void DistanceJoint::accept(ConstraintVisitor& /*cv*/)
+{
+
+}
+
+bool DistanceJoint::references(const std::shared_ptr<SimulationObject>& so)
+{
+    return so == mPointA.getSimulationObject() ||
+            so == mPointB.getSimulationObject();
+}
---
+++ src/simulation/constraints/DistanceJoint.h
@@ -0,0 +1,48 @@
+#ifndef DISTANCEJOINT_H
+#define DISTANCEJOINT_H
+
+#include "Constraint.h"
+#include "data_structures/DataStructures.h"
+
+#include <simulation/references/SimulationPointRef.h>
+
+class RigidBody;
+
+
+// DistanceJoint Constraint:
+// constraints the distance between two points:
+// | x_a - x_b | = d
+// If d == 0, this becomes a BallJoint. BallJoint should still be preferred
+// becaues its more optimized.
+//
+class DistanceJoint : public Constraint
+{
+public:
+    DistanceJoint(SimulationPointRef pointA, SimulationPointRef pointB, double distance = 0.0);
+
+    const Eigen::Vector& getTargetURel() const;
+
+    const Eigen::Vector& getSumOfAllAppliedImpulses() const;
+    void setSumOfAllAppliedImpulses(const Eigen::Vector& impulses);
+
+    // Constraint interface
+public:
+    virtual void initialize(double stepSize) override;
+    virtual bool solve(double maxConstraintError) override;
+    virtual void accept(ConstraintVisitor& cv) override;
+    virtual bool references(const std::shared_ptr<SimulationObject>& so) override;
+
+private:
+
+    SimulationPointRef mPointA;
+    SimulationPointRef mPointB;
+
+    Eigen::Vector mTargetURel;
+    Eigen::Vector mSumOfAllAppliedImpulses;
+    Eigen::Matrix3d mImpulseFactor; // 1 / (K_aa + K_bb)
+
+    double mDistance;
+    double mStepSize;
+};
+
+#endif // DISTANCEJOINT_H
